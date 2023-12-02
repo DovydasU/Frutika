14dfc4f4ffbd230920e0152fece608fbb7809001
DovydasU <ulonasd@gmail.com>
2023-12-02 23:43:58 +0200

    sukurti category reusrsai ir relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function comments()
     {
         return $this->hasMany(Comment::class);
